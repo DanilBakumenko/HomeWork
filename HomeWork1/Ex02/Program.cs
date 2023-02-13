0c72d9387e314171648c3156b4abc6858c519c13
Danil <d.bakumenko@mail.ru>
2023-02-13 15:53:46 +0300

    solved task 2

---
+++ HomeWork1/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿int i = 0;
+int max = 0;
+while (i < 3)
+{
+    Console.WriteLine("Write a number ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (max < a) 
+    {
+        max = a;
+    }
+    i++;
+}
+Console.WriteLine($"Max number = {max}");
+
