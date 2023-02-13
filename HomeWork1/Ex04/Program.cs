fa252e1f6da08e3708d2c890abe2419912e10fe5
Danil <d.bakumenko@mail.ru>
2023-02-13 16:25:10 +0300

    solved task 4

---
+++ HomeWork1/Ex04/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Write a number n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("All numbers in range 1 to n:");
+for (int i = 2; i < (n+1); i = i + 2)
+{
+    Console.Write($"{i} ");
+}
