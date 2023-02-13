14635e5d322b9ae3b53750244f16f0fd85f56bcb
Danil <d.bakumenko@mail.ru>
2023-02-13 16:06:46 +0300

    solved task 3

---
+++ HomeWork1/Ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else 
+{
+    Console.WriteLine("Your number is not even");
+}
