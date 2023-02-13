61cd4701f7724129d6795f8afafb74a9ced046c8
Danil <d.bakumenko@mail.ru>
2023-02-13 15:35:23 +0300

    solved task 1

---
+++ HomeWork1/Ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write number b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b < a)
+{
+    Console.WriteLine($"Min = {b}, Max = {a}");
+}
+else
+{
+    Console.WriteLine($"Min = {a}, Max = {b}");
+}
