d0b8b8b52171d46df63a9117cdc66dc1ca9f3aee
Danil <d.bakumenko@mail.ru>
2023-02-21 13:27:03 +0300

    solved task 19

---
+++ HomeWork3/Ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// Унифицировал алгоритм, программа покажет является ли полиндромом число с любым 
+// количеством цифр
+Console.WriteLine("Write a number");
+int digit = Convert.ToInt32(Console.ReadLine());
+int ReverseNumb(int x)
+{
+    int a = 0;
+    while (x > 0)
+    {
+        a = a*10 + x % 10;
+        x = x/10;
+    }
+    return a;
+}
+int temp = ReverseNumb(digit);
+// Console.WriteLine(temp);
+if (temp == digit)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
