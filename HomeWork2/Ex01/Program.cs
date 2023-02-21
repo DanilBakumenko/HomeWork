f1f0faa21d0134cec609886ed76211c7640a30b3
Danil <d.bakumenko@mail.ru>
2023-02-21 09:59:17 +0300

    tasl 1

---
+++ HomeWork2/Ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    n = n / 10;
+    n = n % 10;
+    Console.WriteLine(n);
+}
+else Console.WriteLine("Eror");
