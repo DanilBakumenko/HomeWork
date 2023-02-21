948c05198899c1b0c02d4ee014db43ec2069f563
Danil <d.bakumenko@mail.ru>
2023-02-21 10:12:46 +0300

    task 2

---
+++ HomeWork2/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. (номер цифры считается от левого края)
+Console.WriteLine("Write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 99)
+{
+        while (n > 1000)
+        {
+            n = n / 10;
+        }
+        Console.WriteLine($"Third number {n % 10}");
+}
+else Console.WriteLine("No third number");
