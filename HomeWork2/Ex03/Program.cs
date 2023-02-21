83518cb324abfc9617670299f5e59b11b30449f9
Danil <d.bakumenko@mail.ru>
2023-02-21 10:20:45 +0300

    task 3

---
+++ HomeWork2/Ex03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+Console.WriteLine("Write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("Today is a day off");
+}
+else if (n > 0 && n < 6)
+{
+    Console.WriteLine("Go to work!");
+}
+else Console.WriteLine("Day does not exist");
