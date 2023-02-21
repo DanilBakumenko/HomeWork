c1cb8983d50aada18c5c1faf022ef58750983dc7
Danil <d.bakumenko@mail.ru>
2023-02-21 13:50:10 +0300

    solved task 23

---
+++ HomeWork3/Ex03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int N = ReadNumber("Enter the digit");
+for (int i = 1; i < N+1; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
