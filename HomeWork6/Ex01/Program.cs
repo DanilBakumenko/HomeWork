d95a87736e2fa05e71669f644e034e54e872c98c
Danil <d.bakumenko@mail.ru>
2023-03-04 14:53:46 +0300

    HomeWork6

---
+++ HomeWork6/Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿double ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    double value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+double b1 = ReadNumber("Enter the b1");
+double k1 = ReadNumber("Enter the k1");
+double b2 = ReadNumber("Enter the b2");
+double k2 = ReadNumber("Enter the k2");
+double x = (b1 - b2)/(k2 - k1);
+double y= k1 * x + b1;
+
+Console.WriteLine($"{x} {y}");
---
+++ HomeWork6/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int M = ReadNumber("How mani numbers you enter");
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    int m = ReadNumber("Enter the number");
+    if (m > 0) count++;
+}
+Console.WriteLine($"You enter {count} numbers > 0");
