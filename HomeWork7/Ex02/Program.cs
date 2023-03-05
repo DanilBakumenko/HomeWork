eaa9749f0239b3393a8a8252c8dc83fcd1818a03
Danil <d.bakumenko@mail.ru>
2023-03-05 11:11:02 +0300

    Solved task 50

---
+++ HomeWork7/Ex02/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+// ​
+// Например, задан массив:
+// ​
+// 1 4 7 2
+// ​
+// 5 9 2 3
+// ​
+// 8 4 2 4
+// ​
+// 1 7 -> такого числа в массиве нет
+// ​
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix(int rows = 10, int columns = 10, int leftBorder = 0, int rightBorder = 10)
+{
+
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Enter the number of row");
+int n = ReadNumber("Enter the number of colum");
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+if (m <= myMatrix.GetLength(0) && n <= myMatrix.GetLength(1))
+{
+    Console.WriteLine($"You found {myMatrix[m, n]}");
+}
+else
+{
+    Console.WriteLine("You've gone beyond the matrix");
+}
