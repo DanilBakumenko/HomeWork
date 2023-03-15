dce75a0e3f5d4105bc1cb6b9762d821930141234
Danil <d.bakumenko@mail.ru>
2023-03-15 15:21:09 +0300

    Solved task 64

---
+++ HomeWork9/Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void PrintNumberForNTo1(int N)
+{
+    if (N > 0)
+    {
+        Console.Write($"{N} ");
+        PrintNumberForNTo1(N - 1);
+    }
+}
+int N = ReadNumber("Enter N");
+PrintNumberForNTo1(N);
